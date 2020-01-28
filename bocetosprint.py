40b3532bc077ff73469f6301e5d580dcf497ac1c
Juana <57723225+jmartineztorrez@users.noreply.github.com>
2020-01-28 16:30:49 +0100

    Add files via upload
    se anexa el iteracion for

--- bocetosprint.py
+++ bocetosprint.py
@@ -3,25 +3,27 @@ class libros:
 	titulo= ''
 	autor= ''
 
-	def consulta(self,titulo,autor): #aqui definimos lo que hara nuestro metodo
+	def consulta(self,titulo,autor):
 		print(self.titulo)
 		print(self.autor)
-#con el siguiente bloque de codigo alojamos en las variables lo que el usuario escribe 
-f = open("text.txt", "r")
+
+
+f = open("prueba.txt", "r")
 print(f.read())
 print("****************************************")
 
 l1= libros()
-l1.titulo=input("Introduce el título: ")
-l1.autor=input("Introduce el autor: ")
+n=int(input("Cuantos Libros Deseas Agregar"))
+for i in range(n):# el ciclo for ejecuta el rango de veces de n, permite agregar mas de un libro
 
+	l1.titulo=input("Introduce el título: ")
+	l1.autor=input("Introduce el autor: ")
+	f = open("prueba.txt", "a")
+	f.write(l1.titulo+" - ")
+	f.write(l1.autor)
+	f.write("\n")
+	f.close()
 
-#Con el siguiente bloque de codigo escribimos usando las variables anteriores al txt
-f = open("text.txt", "a")
-f.write(l1.titulo+" - ")#esta línea escribe el título
-f.write(l1.autor+"\n")#esta línea escribe el autor, el \n crea un salto de línea para la siguiente entrada 
-f.close()#cierra el fichero 
-#abre y lee el fichero después de haber grabado el último libro
-f = open("text.txt", "r")
+f = open("prueba.txt", "r")
 print(f.read())
 f.close()
