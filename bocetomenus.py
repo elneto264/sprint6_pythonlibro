0e134ac6ad1f57920da1160af43a75b9fb47de46
Elneto <ernestoster@gmail.com>
2020-01-29 18:00:37 +0100

    con buscador para titulo

--- bocetomenus.py
+++ bocetomenus.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-class libros:
+class Libro:
 	titulo= ''
 	autor= ''
-
-	def consulta(self,titulo,autor): #aqui definimos lo que hara nuestro metodo
-		print(self.titulo)
-		print(self.autor)
 
 
 	def agregar(self,titulo,autor):
@@ -14,21 +10,31 @@ class libros:
 		n=int(input("Cuantos Libros Deseas Agregar"))#se pregunta cuantos elementos agregar
 		for i in range(n):# el ciclo for ejecuta el rango de veces de n, permite agregar mas de un libro
 		#Con el siguiente bloque de codigo escribimos usando las variables anteriores al txt
-			l1.titulo=input("Introduce el título: ")
-			l1.autor=input("Introduce el autor: ")
+			self.titulo=input("Introduce el título: ")
+			self.autor=input("Introduce el autor: ")
 			f = open("text.txt", "a")
 			f.write(l1.titulo+" - ")#esta línea escribe el título
 			f.write(l1.autor)#esta línea escribe el autor 
 			f.write("\n")#el \n crea un salto de línea para la siguiente entrada
 			f.close()
+	def buscarTitulo(self,titulo,autor): #funccion para busqueda de Titulo, pasamos el parametro de la lista
+		print ("Busca Titulo")
+		#f = open("text.txt", "r")
+    #recorre la lista
+		busqueda = input("Ingrese Titulo : ") #ingresamos el titulo a buscar agregar atributos(titulo y autor)
+		with open("text.txt") as a:
+			if busqueda in a.read():
+				print(busqueda)
+			else:
+				print("no existe")
+
+
 #abre y lee el fichero después de haber grabado el último libro
 
-l1= libros()
+l1= Libro()
 
 #con el siguiente bloque de codigo alojamos en las variables lo que el usuario escribe 
-f = open("text.txt", "r")
-print(f.read())#esta linea lee el fichero
-print("****************************************")
+
 
 
 
@@ -43,6 +49,7 @@ def menu():
 	print ("\t1 - Consultar listado")
 	print ("\t2 - Agregar libro al listado")
 	print ("\t3 - Salir")
+	print ("\t4 - Buscar")
  
  
 while True:
@@ -55,6 +62,10 @@ while True:
 	if opcionMenu=="1":
 		print ("")
 		input("Has pulsado la opción 1...\npulsa una tecla para continuar")
+		f = open("text.txt", "r")
+		print(f.read())#esta linea lee el fichero
+		print("****************************************")
+		#l1.buscarTitulo(l1.titulo,l1.autor)
 
 	elif opcionMenu=="2":
 		print ("")
@@ -62,10 +73,10 @@ while True:
 		l1.agregar(l1.titulo,l1.autor)
 
 	elif opcionMenu=="3":
-		f = open("text.txt", "r")
-		print(f.read())
 		f.close()#cierra el fichero 
 		break
+	elif opcionMenu=="4":
+		l1.buscarTitulo(l1.titulo,l1.autor)
 	else:
 		print ("")
 		input("No has pulsado ninguna opción correcta...\npulsa una tecla para continuar")
